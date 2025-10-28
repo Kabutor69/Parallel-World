b79b599adaf6361d862f71b00a4a5322066e15be
Kabutor69 <pisionz69@gmail.com>
2025-10-28 08:53:25 +0545

    Update channelConfig.js

--- src/models/channelConfig.js
+++ src/models/channelConfig.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 const channelConfigSchema = new mongoose.Schema({
   guildId: { type: String, required: true, unique: true },
+  log: { type: String, default: null },
   birthday: { type: String, default: null },
   invite: { type: String, default: null },
   leave: { type: String, default: null },
-  log: { type: String, default: null },
-  welcome: { type: String, default: null }
+  welcome: { type: String, default: null },
+  updatedBy: { type: String, required: true },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("ChannelConfig", channelConfigSchema);
